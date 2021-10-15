e799fcaf64cfeb4b43141a5d01ce8fa90883c3df
h3R <hermann.plass@gmail.com>
2021-10-15 12:23:50 +0200

    renaming

--- cookies.js
+++ cookies.js
@@ -1,21 +1,20 @@
-export function setCookie(cName, cValue, expDays) {
-    let date = new Date();
-    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
-    document.cookie = `${cName}=${cValue}; expires=${date.toUTCString()}; path=/`;//cName + "=" + cValue + "; " + expires + "; path=/";
+export let cookies = {
+    set: function (cName, cValue, expDays) {
+        let date = new Date();
+        date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+        document.cookie = `${cName}=${cValue}; expires=${date.toUTCString()}; path=/`;
+    },
+
+    get: function (cName) {
+        const name = cName + "=";
+        const cDecoded = decodeURIComponent(document.cookie); //to be careful
+        const cArr = cDecoded.split('; ');
+        let res;x
+        cArr.forEach(val => {
+            if (val.indexOf(name) === 0) res = val.substring(name.length);
+        })
+        return res;
+    }
 }
 
-export function getCookie(cName) {
-    const name = cName + "=";
-    const cDecoded = decodeURIComponent(document.cookie); //to be careful
-    const cArr = cDecoded.split('; ');
-    let res;x
-    cArr.forEach(val => {
-        if (val.indexOf(name) === 0) res = val.substring(name.length);
-    })
-    return res;
-}
-
-Object.assign(window, {
-    setCookie,
-    getCookie
-});
+Object.assign(window, {cookies});
