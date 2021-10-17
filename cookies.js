416d087d41f001d909a0d22095727d20c8fe884f
h3R <hermann.plass@gmail.com>
2021-10-18 01:08:40 +0200

    fixes

--- cookies.js
+++ cookies.js
@@ -1,5 +1,14 @@
-export let cookies = {
+let cookies = {
     set: function (cName, cValue, expDays) {
+
+        let value = cValue;
+        try{
+            switch(cValue.constructor){
+                case String: value = cValue; break;
+                case Object: value = JSON.stringify(cValue);
+            }
+        }catch(e){}
+
         let date = new Date();
         date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
         document.cookie = `${cName}=${cValue}; expires=${date.toUTCString()}; path=/`;
@@ -9,12 +18,16 @@ export let cookies = {
         const name = cName + "=";
         const cDecoded = decodeURIComponent(document.cookie); //to be careful
         const cArr = cDecoded.split('; ');
-        let res;x
+        let res;
         cArr.forEach(val => {
             if (val.indexOf(name) === 0) res = val.substring(name.length);
         })
         return res;
+    },
+
+    remove: function(cName){
+        cookies.set(cName, 0, 0);
     }
 }
-
+export default cookies;
 Object.assign(window, {cookies});
